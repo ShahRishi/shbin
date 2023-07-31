e9d95eb8b87002ae67633d5bcf94ac060b115c68
Rishi Shah <shahrishi108@gmail.com>
2023-07-31 14:41:35 -0700

    implemented unsafe ls

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,27 @@
+use std::fs;
+use dirs;
+use colored::*;
+
 pub fn ls() {
-    println!("You typed ls");
-    // implement the ls functionality here
+    let home_dir = dirs::home_dir().unwrap();
+    let shbin_path = home_dir.join(".shbin");
+
+    let entries = fs::read_dir(shbin_path).unwrap();
+
+    println!("{:<6} {:<20} {:<6}", 
+             "Index".cyan(), 
+             "Name".green(), 
+             "Size".yellow());
+
+    for (i, entry_result) in entries.enumerate() {
+        let entry = entry_result.unwrap();
+        let file_size = format!("{}B", entry.metadata().unwrap().len().to_string()).yellow();
+
+        println!("{:<6} {:<20} {:<6}",
+                 i.to_string().cyan(), 
+                 entry.file_name().to_string_lossy().green(), 
+                 file_size);
+    }
 }
 
 pub fn add(path: &str) {
@@ -15,5 +36,5 @@ pub fn rm(path: &str) {
 
 pub fn push() {
     println!("You typed push");
-    // TODO: implement the push functionality here
+    // TODO:implement the push functionality here
 }
