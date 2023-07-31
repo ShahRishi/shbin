f5e549c999a0e6f5ed770bafe9d47da228b5ad3c
Rishi Shah <shahrishi108@gmail.com>
2023-07-31 13:39:11 -0700

    CLI framework init

---
+++ src/commands.rs
@@ -0,0 +1,19 @@
+pub fn ls() {
+    println!("You typed ls");
+    // implement the ls functionality here
+}
+
+pub fn add(path: &str) {
+    println!("Adding {:?}", path);
+    // implement the add functionality here
+}
+
+pub fn rm(path: &str) {
+    println!("Removing {:?}", path);
+    // implement the rm functionality here
+}
+
+pub fn push() {
+    println!("You typed push");
+    // implement the push functionality here
+}
